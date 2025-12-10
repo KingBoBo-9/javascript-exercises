1e7aa2088c486653a129a06390373eb71f38ddde
KingBoBo-9 <246374481+KingBoBo-9@users.noreply.github.com>
2025-12-10 18:29:21 +0100

    Wrote the reverseString function

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    splitString = string.split("").reverse().join('');
+    return splitString;
 };
+
+
+x = reverseString("This is the string")
+console.log(x);
+
 
 // Do not edit below this line
 module.exports = reverseString;
