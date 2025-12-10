aad645d681620576eaa5e04e85ac3f04e2e29d32
KingBoBo-9 <246374481+KingBoBo-9@users.noreply.github.com>
2025-12-10 18:15:13 +0100

    changed repeatString function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let result = ""
 
+    if (num < 0) {
+        return 'ERROR'
+    } else {
+        for (let index = 0; index < num; index++) {
+            result += string;
+        }
+
+    }
+
+    return result;
 };
+
+x = repeatString('hey', 3); // returns 'heyheyhey'
+console.log(x);
+
 
 // Do not edit below this line
 module.exports = repeatString;
